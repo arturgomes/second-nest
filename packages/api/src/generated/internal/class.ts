6698510b6d1648d4a1261fe7619a06d957dfd4b6
Artur Gomes <artur.o.gomes@gmail.com>
2025-11-23 10:34:39 -0300

    feat: Add .gitignore, and adjust Prisma client generation paths for the API package.

--- packages/api/src/generated/internal/class.ts
+++ packages/api/src/generated/internal/class.ts
@@ -23,7 +23,7 @@ const config: runtime.GetPrismaClientConfig = {
       "value": "prisma-client"
     },
     "output": {
-      "value": "/Users/artur/Documents/studies/second-nest/src/generated",
+      "value": "/Users/artur/Documents/studies/second-nest/packages/api/src/generated",
       "fromEnvVar": null
     },
     "config": {
@@ -37,7 +37,7 @@ const config: runtime.GetPrismaClientConfig = {
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "/Users/artur/Documents/studies/second-nest/prisma/schema.prisma",
+    "sourceFilePath": "/Users/artur/Documents/studies/second-nest/packages/api/prisma/schema.prisma",
     "isCustomOutput": true
   },
   "relativePath": "../../prisma",
