fe0730858eacad7ee5d2ece279fde5d5e8270148
Artur Gomes <artur.o.gomes@gmail.com>
2025-11-23 08:45:15 -0300

    feat: add UsersModule, PostsModule, CommentsModule, and LikesModule to app imports

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 import { UserModule } from './user/user.module';
+import { UsersModule } from './users/users.module';
+import { PostsModule } from './posts/posts.module';
+import { CommentsModule } from './comments/comments.module';
+import { LikesModule } from './likes/likes.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule, UsersModule, PostsModule, CommentsModule, LikesModule],
   controllers: [AppController],
   providers: [AppService],
 })
