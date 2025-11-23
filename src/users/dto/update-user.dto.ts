d75e737a4e1f6db1b6a911f0dbc889089b597760
Artur Gomes <artur.o.gomes@gmail.com>
2025-11-23 09:25:50 -0300

    feat: add Next.js frontend application to `packages/web`.

--- src/users/dto/update-user.dto.ts
+++
@@ -1,9 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateUserDto } from './create-user.dto';
-
-/**
- * Data Transfer Object for updating a user.
- * It extends CreateUserDto but makes all fields optional using PartialType.
- * This allows updating only specific fields without sending the entire object.
- */
-export class UpdateUserDto extends PartialType(CreateUserDto) { }
