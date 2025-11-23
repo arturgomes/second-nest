f9ca8c7a657553ce7b70a5dfc6c3d9acd762884d
Artur Gomes <artur.o.gomes@gmail.com>
2025-11-23 10:33:31 -0300

    feat: style post from listing

--- packages/web/src/components/PostCard.tsx
+++ packages/web/src/components/PostCard.tsx
@@ -24,24 +24,22 @@ interface PostCardProps {
 
 export function PostCard({ post }: PostCardProps) {
   return (
-    <div className="border p-4 rounded">
+    <div className="p-4">
       {/* TODO: Add proper styling with Tailwind */}
       <Link href={`/posts/${post.id}`}>
-        <h2 className="text-xl font-bold">{post.title}</h2>
+        <h2 className="text-4xl font-bold">{post.title}</h2>
       </Link>
 
       {post.content && (
-        <p className="mt-2 text-gray-600">
+        <p className="mt-2 text-xl text-gray-600">
           {/* TODO: Truncate content to preview length */}
-          {post.content.substring(0, 150)}...
+          {post.content.length > 150 ? post.content.substring(0, 150) + '...' : post.content}
         </p>
       )}
 
       <div className="mt-4 text-sm text-gray-500">
-        {/* TODO: Add author info */}
         {post.author && <span>By {post.author.name || post.author.email}</span>}
 
-        {/* TODO: Add like and comment counts */}
         {post._count && (
           <span className="ml-4">
             {post._count.likes} likes • {post._count.comments} comments
@@ -49,7 +47,6 @@ export function PostCard({ post }: PostCardProps) {
         )}
       </div>
 
-      {/* TODO: Add published badge if published */}
       {post.published && <span className="badge">Published</span>}
     </div>
   );
